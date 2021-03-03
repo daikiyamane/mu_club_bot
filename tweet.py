c90966c2a2c06f5f4e8ca716d9242a9b80574dfb
daikiyamane <NovelAppDanzemi@gmail.com>
2021-03-04 02:41:56 +0900

    create some file

---
+++ tweet.py
@@ -0,0 +1,39 @@
+#coding; utf-8
+
+import os
+import tweepy
+
+auth = tweepy.OAuthHandler(
+    os.environ["CONSUMER_KEY"], os.environ["CONSUMER_SECRET"]
+)
+auth.set_access_token(
+    os.environ["ACCESS_TOKEN_KEY"], os.environ["ACCESS_TOKEN_SECRET"]
+)
+api = tweepy.API(auth)
+
+
+def make_accounts():
+    accounts = ["YNe1999"]
+    return accounts
+
+
+def make_tweets(accounts, count, page):
+    return [api.user_timeline(accounts[i], count=count, page=page) for i in range(len(accounts))]
+
+
+def retweet_favorite():
+    accounts = make_accounts()
+    tweets = make_tweets(accounts, 3, 1)
+    for tweet in tweets:
+        for t in tweet:
+            try:
+                api.retweet(t.id)
+            except tweepy.TweepError:
+                print("すでにリツイートしてます")
+            try:
+                api.create_favorite(t.id)
+            except tweepy.TweepError:
+                print("すでにいいねしてます")
+
+
+retweet_favorite()
