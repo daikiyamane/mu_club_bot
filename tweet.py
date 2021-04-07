df888ff1a6bff76f1b24a6688162e864e1c5dd8d
daikiyamane <NovelAppDanzemi@gmail.com>
2021-04-07 19:21:53 +0900

    fix: message

--- tweet.py
+++ tweet.py
@@ -79,6 +79,10 @@ def tweet():
         "・毎日決まった投稿をしたい方\n・「＃春から松山大学」などのハッシュタグを含んだツイートをリツイートいいねしたい方\nそれ自動化しませんか？\n興味がある方はDMください！！\ntweet: {}\n#春から松大 #春から松山大学 #松山大学".format(str_dt_now))
     api.update_status(message2)
 
+    message3 = str("松山大学生の方で情報分野に少しでも興味ある方！\nゲーム開発，アプリ開発，映像制作など自分で何か作りたい方！\nぜひDM下さい！みんなで集まって楽しく活動しましょう{}{}\ntweet: {}\n#春から松大 #春から松山大学 #松山大学".format(
+        chr(int(0x1f601)), chr(int(0x1f601)), str_dt_now))
+    api.update_status(message3)
+
 
 retweet_favorite()
 q_favo(["#春から松大", "#春から松山大学", "#松山大学"], 100)
